d1a60440199761dc6a0a4638fb6829917b7c5980
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-04 17:40:00 -0300

    instalado aries askar para uso do wallet e storage

--- agente/agente_config.tsx
+++ agente/agente_config.tsx
@@ -1,6 +1,8 @@
 import type { InitConfig } from '@aries-framework/core'
 import { Agent } from '@aries-framework/core'
 import { agentDependencies } from '@aries-framework/react-native'
+import { AskarModule } from '@aries-framework/askar'
+import { ariesAskar } from '@hyperledger/aries-askar-react-native'
 
 const config: InitConfig = {
   label: 'docs-agent-react-native',
@@ -11,6 +13,12 @@ const config: InitConfig = {
 }
 
 const agent = new Agent({
-  config,
-  dependencies: agentDependencies,
-})
+    config,
+    dependencies: agentDependencies,
+    modules: {
+      // Register the Askar module on the agent
+      askar: new AskarModule({
+        ariesAskar,
+      }),
+    },
+  })
